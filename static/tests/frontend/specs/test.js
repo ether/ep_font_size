87ea0e3b2be6dd8e612bf20ec2f55c9ba5beabab
John McLear <john@mclear.co.uk>
2020-10-30 17:25:17 +0000

    plz run tests

--- static/tests/frontend/specs/test.js
+++ static/tests/frontend/specs/test.js
@@ -12,11 +12,11 @@ describe("Set Font size and ensure its removed properly", function(){
 
   // Create Pad
   // Select all text
-  // Set it to size red
+  // Set it to size 9
   // Select all text
-  // Set it to size black
+  // Set it to size 8
 
-  it("Changes from size black to red and back to black", function(done) {
+  it("Changes from size 8 to 9 and back to 8", function(done) {
     this.timeout(60000);
     var chrome$ = helper.padChrome$;
     var inner$ = helper.padInner$;
@@ -28,23 +28,23 @@ describe("Set Font size and ensure its removed properly", function(){
     $firstTextElement.sendkeys('foo');
     $firstTextElement.sendkeys('{selectall}');
 
-    // sets first line to Font size red
+    // sets first line to Font size 9
     chrome$('.size-selection').val("1");
     chrome$('.size-selection').change();
 
     var fElement = inner$("div").first();
     helper.waitFor(function(){
-      let elementHasClass = fElement.children().first().hasClass("size:red");
+      let elementHasClass = fElement.children().first().hasClass("size:9");
       return expect(elementHasClass).to.be(true);
     }).done(function(){
       $firstTextElement = inner$("div").first();
       $firstTextElement.sendkeys('{selectall}');
-      // sets first line to Font size black
+      // sets first line to Font size 8
       chrome$('.size-selection').val('0');
       chrome$('.size-selection').change();
       helper.waitFor(function(){
         fElement = inner$("div").first();
-        let elementHasClass = fElement.children().first().hasClass("size:black");
+        let elementHasClass = fElement.children().first().hasClass("size:8");
         return expect(elementHasClass).to.be(true);
       }).done(function(){
         done();
