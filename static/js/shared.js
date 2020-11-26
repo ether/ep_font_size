1d897b24fd609c7102e74634c33800b926194bf0
Richard Hansen <rhansen@rhansen.org>
2020-11-26 17:38:22 -0500

    Factor out duplicated size list

--- static/js/shared.js
+++ static/js/shared.js
@@ -4,3 +4,29 @@ exports.collectContentPre = function (hook, context) {
     context.cc.doAttrib(context.state, `font-size:${size[1]}`);
   }
 };
+
+exports.sizes = [
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
+  '22',
+  '24',
+  '26',
+  '28',
+  '30',
+  '35',
+  '40',
+  '45',
+  '50',
+  '60',
+];
